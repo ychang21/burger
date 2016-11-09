8c5b872077dbad3d53a4e888019bdc3f9af1fc1d
ychang21 <isabellhchang@gmail.com>
2016-11-09 10:20:44 -0500

    Adding JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,17 @@
 //connection to database and export to be used for orm
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-	port: 3306,
-	host: 'localhost',
-	user: 'root',
-	password: '',
-	database: 'burgers_db'
-});
+var connection;
 
-connection.connect(function (err) {
-	if (err) {
-		console.error('error connecting: ' + err.stack);
-		return;
-	}
-	console.log('connected as id ' + connection.threadId);
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+}else {
+    mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'burgers_db'
+  });
+};
 
+connection.connect();
 module.exports = connection;
